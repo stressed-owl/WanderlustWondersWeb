1a15c3a7e12d58438e32cfc971d50f6e042749d1
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-09-03 16:14:43 +0300

    Deleted unnecessary files and components

--- wanderlust-wonders-frontend/src/components/commons/textfields/authentication/AuthTextField.jsx
+++
@@ -1,9 +0,0 @@
-import { TextField } from "@mui/material";
-import { styled } from "@mui/material/styles";
-
-const AuthTextField = styled(TextField)(() => ({
-  width: 450,
-  fontFamily: "Open Sans",
-}));
-
-export default AuthTextField;
