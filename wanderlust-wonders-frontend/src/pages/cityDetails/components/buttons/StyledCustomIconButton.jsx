d310e72c826bffce958c6db4764bdadb5898f1ed
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-09-03 16:30:10 +0300

    Added a custom tooltip for share icon button. The content of a tooltip was also changed

--- wanderlust-wonders-frontend/src/pages/cityDetails/components/buttons/StyledCustomIconButton.jsx
+++ wanderlust-wonders-frontend/src/pages/cityDetails/components/buttons/StyledCustomIconButton.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import CustomIconButton from "./CustomIconButton";
-import { Tooltip } from "@mui/material";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import { styled } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+
+const CustomTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "#E9AFB9",
+    color: "#000",
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    fontFamily: "Open Sans",
+    fontWeight: "400",
+    borderRadius: "8px",
+    padding: "8px 12px",
+  },
+}));
 
 const StyledCustomIconButton = ({ onClick, children }) => {
   return (
-    <Tooltip title="Share">
+    <CustomTooltip
+      title={
+        <React.Fragment>
+          <Typography color="inherit">Share</Typography>
+          {"Allows you to share a link of a city with your friends"}
+        </React.Fragment>
+      }
+    >
       <CustomIconButton onClick={onClick}>{children}</CustomIconButton>
-    </Tooltip>
+    </CustomTooltip>
   );
 };
 
